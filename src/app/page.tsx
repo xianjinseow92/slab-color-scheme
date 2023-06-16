0eabcf649a25ee8432694b7b7cecce9111551d30
Xian Jin Seow <xianjin.seow@circles.asia>
2023-06-16 18:58:30 +0800

    Install antd as design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { Layout } from "antd";
 
 export default function Page() {
-  return <h1>Hello!</h1>
+  return (
+    <Layout style={{ width: "100%", height: "100vh" }}>
+      <h1>Hello!</h1>
+    </Layout>
+  );
 }
